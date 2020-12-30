bcd2a8f8ed1d005f8c5910b2e79bc29eafa4ee19
Zeynep Usta <z.usta@hymnai.com>
2020-12-30 23:36:12 +0000

    FeatureRunner.java and StripeAPI.feature is added

---
+++ src/test/java/Features/FeatureRunner.java
@@ -0,0 +1,14 @@
+package Features;
+
+import com.intuit.karate.KarateOptions;
+import com.intuit.karate.junit4.Karate;
+import org.junit.runner.RunWith;
+
+@RunWith(Karate.class)
+
+@KarateOptions(
+        tags = "@wip"
+)
+
+public class FeatureRunner {
+}
